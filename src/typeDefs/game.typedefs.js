c48f66c9a6c406074757ee638f367664b8131608
Zyzto <Zyzto@pm.me>
2020-07-30 22:54:07 +0300

    TypeDefs: User Removed unwanted features

--- src/typeDefs/game.typedefs.js
+++ src/typeDefs/game.typedefs.js
@@ -4,39 +4,16 @@ export default gql`
     extend type Query {
         games: [Game!]!
         game(id: ID!): Game!
-        roomCount(id: ID!): Int!
     }
     extend type Mutation {
-        newGame(
-            name: String!
-            isScorable: Boolean!
-            gameDetails: String!
-            playersLimit: Int!
-            time: Int!
-            img: String!
-        ): Game
-        editGame(
-            id: ID!
-            name: String
-            isScorable: Boolean
-            time: Int
-            gameDetails: String
-            playersLimit: Int
-            img: String
-        ): Game
+        newGame(name: String!, maxPlayers: Int!, img: String!): Game
+        editGame(id: ID!, name: String, maxPlayers: Int, img: String): Game
         deleteGame(id: ID!): String!
     }
     type Game {
         id: ID!
         name: String!
-        isScorable: Boolean!
-        gameDetails: String!
-        playersLimit: Int
-        time: Int!
+        maxPlayers: Int
         img: String!
-        # count(id: ID!): RoomCount!
     }
-    # type RoomCount {
-    #     count: Int
-    # }
 `
